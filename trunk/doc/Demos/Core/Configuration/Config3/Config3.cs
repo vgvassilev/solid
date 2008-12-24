c02697a0ecdbaf463bc427f5eb2c59c5ac2135fd
vgvassilev <v.g.vassilev@gmail.com>
2008-12-24 10:58:40 +0000

    Separate configuration added to be used by ConfigDemo.

---
+++ trunk/doc/Demos/Core/Configuration/Config3/Config3.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 13.10.2008 г.
+ * Time: 13:03
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+namespace SeparateConfig
+{
+	/// <summary>
+	/// Description of Config3.
+	/// </summary>
+	sealed public class Config3
+	{
+		public static float a = 2;
+		public static float b = 8;
+		public static float h = 2;
+		
+		public static string str = "Surface of trapezium";	
+	}
+	
+	
+}
