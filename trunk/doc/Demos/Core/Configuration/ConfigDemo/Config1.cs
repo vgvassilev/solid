66eda86a1859f6d2bcd406090cb79964ac07d292
vgvassilev <v.g.vassilev@gmail.com>
2008-12-24 10:53:34 +0000

    The example demonstrates how to manage Mono.Cecil when iterating the assembly metadata. It simply traverses the Config3.dll, changes the default value of the str field and saves it into Config3.modified.dll. The demo also prints method names and so on.

---
+++ trunk/doc/Demos/Core/Configuration/ConfigDemo/Config1.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 13.10.2008 г.
+ * Time: 12:45
+ * 
+ */
+
+using System;
+
+namespace ConfigDemo
+{
+	/// <summary>
+	/// Description of Config1.
+	/// </summary>
+	sealed public class Config1// : ConfigParams
+	{
+		public Config1()
+		{
+		}
+		public static float a = 7.5f;
+		public static float b = 3;
+		public static string str = "Surface of the rect";
+		
+		
+	}
+}
