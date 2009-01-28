2ba7f36344040b7b98b4449e69a487d9cfd5fa2a
vgvassilev <v.g.vassilev@gmail.com>
2009-01-28 18:27:57 +0000

    Test cases added to project

---
+++ trunk/SolidOpt/test/MyClass.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 28.1.2009 г.
+ * Time: 15:58
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace test
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	[TestFixture]
+	public class MyClass
+	{
+		[Test]
+		public void TestMethod1()
+		{
+			int a = 5;
+			if (a == 6)
+				a = 7;
+			Assert.AreEqual(a, 5);
+		}
+	}
+}
