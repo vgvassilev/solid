39fefa225711bd0cc44bf4f0aa6fb8a91c054c2c
vgvassilev <v.g.vassilev@gmail.com>
2009-06-24 12:31:57 +0000

    Extension structure created. Here should be kept the plug-ins. Transformers folder added. Here should be kept the (model) transformations and optimizations.

---
+++ trunk/SolidOpt/src/Optimizer/Transformers/ITransform.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 15:18
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+using SolidOpt.Core.Services;
+
+namespace SolidOpt.Optimizer.Transformers
+{
+	/// <summary>
+	/// Description of ITransform.
+	/// </summary>
+	public interface ITransform<Source, Target> : IService
+	{
+		Target Transform(Source source);
+	}
+	
+	public interface ITransform<Target> : ITransform<Target, Target>
+	{
+	}
+}
