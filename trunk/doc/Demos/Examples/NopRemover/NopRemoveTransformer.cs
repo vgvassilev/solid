8b339b5c87aa247811f56859ac4d4ba4186ed1b3
vgvassilev <v.g.vassilev@gmail.com>
2009-06-26 09:41:23 +0000

    Plug-in for removing nop instructions added.

---
+++ trunk/doc/Demos/Examples/NopRemover/NopRemoveTransformer.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 17:23
+ * 
+ */
+using System;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using SolidOpt.Optimizer.Transformers;
+
+namespace NopRemover
+{
+	/// <summary>
+	/// Description of NopRemoveTransformer.
+	/// </summary>
+	public class NopRemoveTransformer : ITransform<MethodDefinition>
+	{
+		public NopRemoveTransformer()
+		{
+		}
+		
+		public MethodDefinition Transform(MethodDefinition source)
+		{
+			
+			CilWorker cil = source.Body.CilWorker;
+			foreach (Instruction instr in source.Body.Instructions) {
+				if (instr.OpCode == OpCodes.Nop)
+					cil.Remove(instr);
+			}
+			return source;
+		}
+	}
+}
---
+++ trunk/doc/Demos/Examples/NopRemover/post-build.bat
@@ -0,0 +1,2 @@
+copy "NopRemover.dll" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
+copy "NopRemover.pdb" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
