2ec53ddd3977801f46fa085ad6667ed6498b0259
vgvassilev <v.g.vassilev@gmail.com>
2009-06-26 09:42:29 +0000

    Standard Loader renamed to StandardLoader

---
+++ trunk/doc/Demos/Examples/StandardLoader/Loadee/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 16:47
+ * 
+ */
+using System;
+
+namespace Loadee
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Triangle();
+			
+			Console.ReadKey(true);
+		}
+		
+		public static void Triangle()
+		{
+			int a = 5;
+			int b = 8;
+			int c = 10;
+			int P = a + b + c;
+			Console.WriteLine("Perimeter is {0}", P);
+			
+			int p = P/2;
+			double S = Math.Sqrt(p * (p-a) * (p-b) * (p-c));
+			Console.WriteLine("Area is {0}", S);
+		}
+	}
+}
---
+++ trunk/doc/Demos/Examples/StandardLoader/Loader/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 15:52
+ * 
+ */
+using System;
+using System.Diagnostics;
+
+namespace Loader
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			Console.WriteLine("Hello Loader!");
+			
+			Trace.Listeners.Add(new ConsoleTraceListener());
+			
+			int result = new SolidOpt.Core.Loader.Loader().Run(args);
+			
+			Console.ReadKey(true);
+			return result;
+			
+		}
+	}
+}
