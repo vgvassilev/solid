1a353f4325ac967230dbc21f8298c0b05a207616
vgvassilev <v.g.vassilev@gmail.com>
2009-06-26 09:39:08 +0000

    Loader extended. Visitors for method traverse added.

--- trunk/SolidOpt/src/Core/Loader/Loader.cs
+++ trunk/SolidOpt/src/Core/Loader/Loader.cs
@@ -1,33 +1,112 @@
 using System;
+using System.IO;
+using System.Diagnostics;
+
+using Mono.Cecil.Cil;
+using Mono.Cecil;
 
 using SolidOpt.Core.Services;
+using SolidOpt.Optimizer.Transformers;
 
 namespace SolidOpt.Core.Loader
 {
 	public class Loader
 	{
-		public PluginServiceContainer servicesContainer;
-
+		private PluginServiceContainer servicesContainer = new PluginServiceContainer();
+		public PluginServiceContainer ServicesContainer {
+			get { return servicesContainer; }
+			set { servicesContainer = value; }
+		}
+		
 		public Loader()
 		{
-			Console.WriteLine("Initialize...");
-			servicesContainer = new PluginServiceContainer();
+			Trace.WriteLine("Initialize...");
 		}
 
 		public int Run(string[] args)
 		{
+			if (args.Length < 1) return -1;
+			
 			foreach(string s in args) Console.WriteLine(s);
-			Console.WriteLine("Search plugins...");
+			Trace.WriteLine("Search plugins...");
 			
-			servicesContainer.AddPlugins(AppDomain.CurrentDomain.BaseDirectory + "core");
-			servicesContainer.AddPlugins(AppDomain.CurrentDomain.BaseDirectory + "plugins");
-			
-			foreach(PluginInfo p in servicesContainer.plugins) Console.WriteLine(p.fullName);
-			Console.WriteLine("Load plugins...");
-			servicesContainer.LoadPlugins();
-			Console.WriteLine("Configure plugins...");
+			ServicesContainer.AddPlugins(AppDomain.CurrentDomain.BaseDirectory + "core");
+			ServicesContainer.AddPlugins(AppDomain.CurrentDomain.BaseDirectory + "plugins");
+						
+			foreach(PluginInfo p in ServicesContainer.plugins) Trace.WriteLine(p.fullName);
+			Trace.WriteLine("Load plugins...");
+			ServicesContainer.LoadPlugins();
+			Trace.WriteLine("Configure plugins...");
+			Optimize(args);
 			//
 			return 0;
 		}
+		
+		internal void Optimize(string[] args)
+		{
+			
+			IService[] transformers = (IService[]) ServicesContainer.GetServices(typeof(ITransform<MethodBody>));
+			
+			AssemblyDefinition assembly = AssemblyFactory.GetAssembly(args[0]);
+			assembly.MainModule.Accept(new StructureVisitor(transformers));
+			AssemblyFactory.SaveAssembly(assembly, Path.ChangeExtension(args[0],
+			                                                            ".modified" + Path.GetExtension(args[0])));
+			
+		}
+		
+		internal class StructureVisitor: BaseStructureVisitor
+		{
+			private IService[] transformers;
+			
+			public StructureVisitor(IService[] transformers) : base()
+			{
+				this.transformers = transformers;
+			}
+			
+			public override void VisitModuleDefinition(ModuleDefinition module)
+			{
+				module.Accept(new ReflectionVisitor(transformers));
+			}
+		}
+	
+		internal class ReflectionVisitor: BaseReflectionVisitor
+		{
+			private IService[] transformers;
+			
+			public ReflectionVisitor(IService[] transformers) : base()
+			{
+				this.transformers = transformers;
+			}
+			
+			public override void VisitTypeDefinitionCollection(TypeDefinitionCollection types)
+			{
+				base.VisitTypeDefinitionCollection(types);
+				foreach (TypeDefinition type in types)
+					type.Accept(this);
+			}
+			
+			public override void VisitTypeDefinition(TypeDefinition type)
+			{
+				base.VisitTypeDefinition(type);
+			}
+			
+			public override void VisitMethodDefinitionCollection(MethodDefinitionCollection methods)
+			{
+				base.VisitMethodDefinitionCollection(methods);
+				foreach (MethodDefinition method in methods)
+					method.Accept(this);
+			}
+			
+			
+			public override void VisitMethodDefinition(MethodDefinition method)
+			{
+				base.VisitMethodDefinition(method);
+				Trace.WriteLine(method.Name);
+				
+				foreach (ITransform<MethodDefinition> transformer in transformers)
+					method = transformer.Transform(method);
+			}		
+		}
+		
 	}
 }
