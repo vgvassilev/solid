cb95e58e2dc893e29cd1c16395068b342128f0f3
alexander_penev <alexander_penev@yahoo.com>
2009-06-29 10:19:08 +0000

    Add Triangle1 method with DO-WHILE and IF statements

--- trunk/doc/Demos/Examples/StandardLoader/Loadee/Program.cs
+++ trunk/doc/Demos/Examples/StandardLoader/Loadee/Program.cs
@@ -14,6 +14,7 @@ namespace Loadee
 		public static void Main(string[] args)
 		{
 			Triangle();
+			Triangle1();
 			
 			Console.ReadKey(true);
 		}
@@ -29,6 +30,28 @@ namespace Loadee
 			int p = P/2;
 			double S = Math.Sqrt(p * (p-a) * (p-b) * (p-c));
 			Console.WriteLine("Area is {0}", S);
+		}
+		
+		public static void Triangle1()
+		{
+			int a;
+			int b;
+			int c;
+			int P;
+			do {
+				a = 5;
+				b = 8;
+				c = 10;
+				P = a + b + c;
+				Console.WriteLine("Perimeter is {0}", P);
+				
+				int p = P/2;
+				double S = Math.Sqrt(p * (p-a) * (p-b) * (p-c));
+				if (S == 0)
+					Console.WriteLine("Area is Zero");
+				else
+					Console.WriteLine("Area is {0}", S);
+			} while (a == 5);
 		}
 	}
 }
