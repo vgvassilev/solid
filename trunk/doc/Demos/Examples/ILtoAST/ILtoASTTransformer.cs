c101650ebf9dcf199485034dffefb7d881637a25
vgvassilev <v.g.vassilev@gmail.com>
2009-07-02 12:35:35 +0000

    Initial version of transformer from MethodBody to AstMethodDefinition created using Cecil.Decompiler project.

---
+++ trunk/doc/Demos/Examples/ILtoAST/ILtoASTTransformer.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 17:23
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using Cecil.Decompiler;
+using Cecil.Decompiler.Cil;
+using Cecil.Decompiler.Languages;
+
+using SolidOpt.Optimizer.Transformers;
+
+namespace ILtoAST
+{
+	/// <summary>
+	/// Description of NopRemoveTransformer.
+	/// </summary>
+	public class ILtoASTTransformer : ITransform<MethodDefinition, AstMethodDefinition>
+	{
+		public ILtoASTTransformer()
+		{
+		}
+
+		public AstMethodDefinition Transform(MethodDefinition source)
+		{
+			CSharp csharpLang = new CSharp();
+			DecompilationPipeline pipeline = csharpLang.CreatePipeline();
+			pipeline.Run(source.Body);
+			
+			return new AstMethodDefinition(source, pipeline.Body);
+		}
+	}
+}
---
+++ trunk/doc/Demos/Examples/ILtoAST/post-build.bat
@@ -0,0 +1,2 @@
+copy "ILtoAST.dll" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
+copy "ILtoAST.pdb" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
