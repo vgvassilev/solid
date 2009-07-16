42cb317edf10371c0a78bf8411245f47ee865075
vgvassilev <v.g.vassilev@gmail.com>
2009-07-16 11:19:04 +0000

    AstMethodDefinitin classes moved in separate project. New extended decompilation pipeline added. A method for formatting the control flow graph created.

--- trunk/doc/Demos/Examples/ILtoAST/ILtoASTTransformer.cs
+++ trunk/doc/Demos/Examples/ILtoAST/ILtoASTTransformer.cs
@@ -5,6 +5,7 @@
  * Time: 17:23
  * 
  */
+ 
 using System;
 using System.Collections.Generic;
 
@@ -14,6 +15,8 @@ using Mono.Cecil.Cil;
 using Cecil.Decompiler;
 using Cecil.Decompiler.Cil;
 using Cecil.Decompiler.Languages;
+
+using AstMethodDefinitionModel;
 
 using SolidOpt.Optimizer.Transformers;
 
@@ -30,11 +33,55 @@ namespace ILtoAST
 
 		public AstMethodDefinition Transform(MethodDefinition source)
 		{
-			CSharp csharpLang = new CSharp();
+			ILanguage csharpLang = CSharp.GetLanguage(CSharpVersion.V1);//new CSharp();
 			DecompilationPipeline pipeline = csharpLang.CreatePipeline();
 			pipeline.Run(source.Body);
 			
+			
+			
+			var cfg = ControlFlowGraph.Create (source);
+
+			FormatControlFlowGraph (Console.Out, cfg);
+
+//			Console.WriteLine ("--------------------");
+
+//			var store = AnnotationStore.CreateStore (cfg, BlockOptimization.Detailed);
+//			PrintAnnotations (method, store);
+
+//			var language = CSharp.GetLanguage (CSharpVersion.V1);
+			
+
+			//var body = method.Body.Decompile (language);
+
+			var writer = csharpLang.GetWriter (new PlainTextFormatter (Console.Out));
+
+			writer.Write (source);
+			
+			
+			
 			return new AstMethodDefinition(source, pipeline.Body);
+		}
+		
+		public static void FormatControlFlowGraph (System.IO.TextWriter writer, ControlFlowGraph cfg)
+		{
+			foreach (InstructionBlock block in cfg.Blocks) {
+				writer.WriteLine ("block {0}:", block.Index);
+				writer.WriteLine ("\tbody:");
+				foreach (Instruction instruction in block) {
+					writer.Write ("\t\t");
+					var data = cfg.GetData (instruction);
+					writer.Write ("[{0}:{1}] ", data.StackBefore, data.StackAfter);
+					Formatter.WriteInstruction (writer, instruction);
+					writer.WriteLine ();
+				}
+				InstructionBlock [] successors = block.Successors;
+				if (successors.Length > 0) {
+					writer.WriteLine ("\tsuccessors:");
+					foreach (InstructionBlock successor in successors) {
+						writer.WriteLine ("\t\tblock {0}", successor.Index);
+					}
+				}
+			}
 		}
 	}
 }
--- trunk/doc/Demos/Examples/ILtoAST/post-build.bat
+++ trunk/doc/Demos/Examples/ILtoAST/post-build.bat
@@ -1,2 +1,1 @@
-copy "ILtoAST.dll" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
-copy "ILtoAST.pdb" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\"
+copy "*.*" "..\..\..\StandardLoader\Loader\bin\Debug\plugins\" /y
