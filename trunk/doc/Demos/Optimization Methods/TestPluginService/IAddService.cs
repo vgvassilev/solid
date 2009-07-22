a943401c4b114f22ae71a4dbfd212163fcf52ce5
vgvassilev <v.g.vassilev@gmail.com>
2009-07-22 11:42:14 +0000

    Test plug-in services refined.

---
+++ trunk/doc/Demos/Optimization Methods/TestPluginService/IAddService.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vassil Vassilev
+ * Date: 24.6.2009 г.
+ * Time: 11:37
+ * 
+ */
+using System;
+
+using SolidOpt.Core.Services;
+
+namespace TestPluginService
+{
+	/// <summary>
+	/// Description of IAddService.
+	/// </summary>
+	public interface IAddService : IService
+	{
+		
+		int Add(int a, int b);
+		
+	}
+}
