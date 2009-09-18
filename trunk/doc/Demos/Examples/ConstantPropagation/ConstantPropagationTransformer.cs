285d12d10d80b97383c87bc9c4ebecac24aee3d7
vgvassilev <v.g.vassilev@gmail.com>
2009-09-18 18:32:01 +0000

    Constant Propagation method added

---
+++ trunk/doc/Demos/Examples/ConstantPropagation/ConstantPropagationTransformer.cs
@@ -0,0 +1,60 @@
+﻿/*
+ *
+ * User: Vassil Vassilev
+ * Date: 18.9.2009 г.
+ * Time: 16:28
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Cecil.Decompiler.Ast;
+
+using AstMethodDefinitionModel;
+using ILtoAST;
+
+using SolidOpt.Optimizer.Transformers;
+
+namespace ConstantPropagation
+{
+	/// <summary>
+	/// Description of ConstantPropagationTransformer.
+	/// </summary>
+	public class ConstantPropagationTransformer : BaseCodeTransformer, ITransform<AstMethodDefinition>
+	{
+		
+		private var substitutions = new Dictionary<Expression, Expression>();
+		
+		public ConstantPropagationTransformer()
+		{
+		}
+		
+		public AstMethodDefinition Transform(AstMethodDefinition source)
+		{
+			source.Block = (BlockStatement) Visit(source.Block);
+			return source;
+		}
+		
+		public override ICodeNode VisitAssignExpression(AssignExpression node)
+		{
+			base.VisitAssignExpression(node);
+//			if (node.Target is VariableReferenceExpression ||)
+			
+			
+		}
+		
+		public override ICodeNode VisitArgumentReferenceExpression(ArgumentReferenceExpression node)
+		{
+			return base.VisitArgumentReferenceExpression(node);
+		}
+		
+		public override ICodeNode VisitVariableReferenceExpression(VariableReferenceExpression node)
+		{
+			return base.VisitVariableReferenceExpression(node);
+		}
+		
+		
+	}
+}
