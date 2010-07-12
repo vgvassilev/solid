6fad5d6a595369c5490fc129dd566b34527a3b3c
alexander_penev <alexander_penev@yahoo.com>
2010-07-12 15:51:43 +0000

    Removed usage of BaseReflectionVisitor and BaseStructureVisitor (Mono.Cecil 0.9.3 migration)

--- trunk/core/src/Services/Subsystems/Configurator/Sources/ILSource.cs
+++ trunk/core/src/Services/Subsystems/Configurator/Sources/ILSource.cs
@@ -51,8 +51,6 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 	/// </summary>
 	public class ILSource<TParamName> : IConfigSource<TParamName>
 	{
-		internal static Dictionary<TParamName, object> dict = new Dictionary<TParamName, object>();
-		
 		public ILSource()
 		{
 			Trace.Listeners.Add(new ConsoleTraceListener());
@@ -67,12 +65,91 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 		{
 			//Mono 0.9 migration: AssemblyDefinition assembly = AssemblyFactory.GetAssembly(resStream);
 			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(resStream);
-			assembly.MainModule.Accept(new StructureVisitor<TParamName>());
+			//Mono 0.9 migration: assembly.MainModule.Accept(new StructureVisitor<TParamName>());
+			Dictionary<TParamName, object> dict = new Dictionary<TParamName, object>();
+			foreach(ModuleDefinition module in assembly.Modules) {
+				foreach(TypeDefinition type in module.Types) {
+					if (type.Name != "<Module>") {
+						if (type.Name != "Global") {
+							Dictionary<TParamName, object> subDict = new Dictionary<TParamName, object>();
+							VisitConfigTypes(type, subDict);
+							dict[(TParamName)Convert.ChangeType(type.Name, typeof(TParamName))] = subDict;
+						} else {
+							VisitConfigTypes(type, dict);
+						}
+					}
+				}
+			}
 			
 			return dict;
 		}
+		
+		internal void VisitConfigTypes(TypeDefinition type, Dictionary<TParamName, object> dict)
+		{
+			foreach (FieldDefinition field in type.Fields) {
+				dict[(TParamName)Convert.ChangeType(field.Name, typeof(TParamName))] = GetFieldValue(type.Methods, field);
+			}
+			
+			foreach (TypeDefinition nestedType in type.NestedTypes) {
+				Dictionary<TParamName, object> subDict = new Dictionary<TParamName, object>();
+				VisitConfigTypes(nestedType, subDict);
+				dict[(TParamName)Convert.ChangeType(nestedType.Name, typeof(TParamName))] = subDict;
+			}
+		}
+		
+		internal object GetFieldValue(Collection<MethodDefinition> collection, FieldDefinition field)
+		{
+			foreach (MethodDefinition cctor in collection) {
+				if (cctor.IsConstructor && cctor.IsStatic && cctor.IsHideBySig) {
+					foreach (Instruction instr in cctor.Body.Instructions) {
+						if (instr.Next.Operand != null && 
+						    (instr.Next.OpCode == OpCodes.Stsfld &&
+						    (instr.Next.Operand as FieldDefinition).Name == field.Name)) {
+							switch (instr.OpCode.Code) {
+								case Code.Ldstr : 
+									return instr.Operand;
+								case Code.Ldc_I4_M1 :
+									return -1;
+								case Code.Ldc_I4_0 :
+									return 0;
+								case Code.Ldc_I4_1 :
+									return 1;
+								case Code.Ldc_I4_2 :
+									return 2;
+								case Code.Ldc_I4_3 :
+									return 3;
+								case Code.Ldc_I4_4 :
+									return 4;
+								case Code.Ldc_I4_5 :
+									return 5;
+								case Code.Ldc_I4_6 :
+									return 6;
+								case Code.Ldc_I4_7 :
+									return 7;
+								case Code.Ldc_I4_8 :
+									return 8;
+									
+								case Code.Ldc_I4_S :
+								case Code.Ldc_I4 :
+								case Code.Ldc_I8 :
+									return instr.Operand;
+								case Code.Ldnull :
+									return null;
+								case Code.Ldc_R4 :
+								case Code.Ldc_R8 :
+									return instr.Operand;
+									
+							}
+						}
+					}
+				}
+			}
+			return null;
+		}
 	}
 	
+	//Mono 0.9 migration: 
+	/*
 	#region Visitors
 	internal class StructureVisitor<TParamName>: BaseStructureVisitor
 	{
@@ -81,7 +158,7 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 			module.Accept(new ReflectionVisitor<TParamName>());
 		}
 	}
-		
+
 	internal class ReflectionVisitor<TParamName>: BaseReflectionVisitor
 	{
 		internal bool isFirstLevel = true;
@@ -90,7 +167,6 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 		{
 			base.VisitCollection(types);
 		}
-		
 		
 		public override void VisitTypeDefinition(TypeDefinition type)
 		{
@@ -124,7 +200,7 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 		internal object GetFieldValue(Collection<MethodReference> collection, FieldDefinition field)
 		{
 			foreach (MethodDefinition cctor in collection) {
-				if (cctor.IsConstructor && cctor.IsStatic && /*cctor.IsSpecialName*/ && cctor.IsHideBySig) {
+				if (cctor.IsConstructor && cctor.IsStatic && cctor.IsSpecialName && cctor.IsHideBySig) {
 					foreach (Instruction instr in cctor.Body.Instructions) {
 						if (instr.Next.Operand != null && 
 						    (instr.Next.OpCode == OpCodes.Stsfld &&
@@ -172,4 +248,5 @@ namespace SolidOpt.Services.Subsystems.Configurator.Sources
 		}
 	}
 	#endregion
+	*/
 }
