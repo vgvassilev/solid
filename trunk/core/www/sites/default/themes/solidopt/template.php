2fad4e2d78d352c880bef701f15aa62334c7c93e
vgvassilev <v.g.vassilev@gmail.com>
2010-08-05 13:30:21 +0000

    "Submitted by :" to node restyled.

--- trunk/core/www/sites/default/themes/solidopt/template.php
+++ trunk/core/www/sites/default/themes/solidopt/template.php
@@ -1,6 +1,12 @@
-<?php function phptemplate_menu_item_link($link) {
+<?php 
+function phptemplate_menu_item_link($link) {
   if (function_exists('menuclass_to_link')) {
     menuclass_to_link($link);
   }
   return theme_menu_item_link($link);
 }
+
+function SolidOptTheme_preprocess_node_submitted($node) {
+
+}
+?>
