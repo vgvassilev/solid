8137aab36194536a3853a4d1338b82f074c9b075
vgvassilev <v.g.vassilev@gmail.com>
2010-10-06 15:57:48 +0000

    Dead code elimination optimization method added.

---
+++ trunk/src/Services/Transformations/Optimizations/CFG/DeadCodeElimination/src/DeadCodeTransformer.cs
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ */
+
+using System;
+using System.Collections.Generic;
+
+using SolidOpt.Services.Transformations.Optimizations;
+using SolidOpt.Services.Transformations.CodeModel.ControlFlowGraph;
+
+namespace SolidOpt.Services.Transformations.Optimizations.CFG.DeadCodeElimination
+{
+	public class DeadCodeTransformer : IOptimize<ControlFlowGraph>
+	{
+		
+		public DeadCodeTransformer ()
+		{
+		}
+		
+		#region IOptimize<ControlFlowGraph> implementation
+		public ControlFlowGraph Optimize (ControlFlowGraph source)
+		{
+			throw new System.NotImplementedException();
+		}
+		
+		#endregion
+		
+	}
+}
+
