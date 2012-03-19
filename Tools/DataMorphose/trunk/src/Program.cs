e2c3c4a75c11c0849b9b492e6146f2da48f2d890
vgvassilev <v.g.vassilev@gmail.com>
2012-03-19 11:25:45 +0000

    Lay down the new empty project waiting to be filled-in by Petya...

---
+++ Tools/DataMorphose/trunk/src/Program.cs
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+
+using System;
+
+namespace DataMorphose
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			// Implementation still to come
+			return 0;
+		}
+	}
+}
+
