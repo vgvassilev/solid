f32256fb96742b76e2b86c802b95f2308b201901
vgvassilev <v.g.vassilev@gmail.com>
2012-03-29 09:22:54 +0000

    Add first implementation using both Gtk and Windows.Forms. It is extremly crappy.

--- Tools/SolidReflector/trunk/src/MainWindow.cs
+++ Tools/SolidReflector/trunk/src/MainWindow.cs
@@ -8,7 +8,7 @@ using System;
 using Gtk;
 
 public partial class MainWindow: Gtk.Window
-{	
+{
 	public MainWindow(): base(Gtk.WindowType.Toplevel)
 	{
 		Build();
@@ -20,12 +20,31 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 
-	protected void OnOpenActionActivated (object sender, System.EventArgs e)
+	protected void OnOpenActionActivated(object sender, System.EventArgs e)
 	{
-		System.Windows.Forms.MessageBox.Show("It works!");
+		string[] fileNames = {};
+		System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+		ofd.Multiselect = true;
+		ofd.CheckFileExists = true;
+		ofd.InitialDirectory = Environment.CurrentDirectory;
+		if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+			fileNames = ofd.FileNames;
+		}
+		
+		TreeViewColumn col = new TreeViewColumn();
+		assemblyView.AppendColumn(col);
+		
+		ListStore ls = new ListStore(typeof(string));
+		foreach (string file in fileNames) {
+			ls.AppendValues(file);	
+		}
+
+		assemblyView.Model = ls;
+		assemblyView.ShowAll();
+		//System.Windows.Forms.MessageBox.Show("It works!");
 	}
 
-	protected void OnExitActionActivated (object sender, System.EventArgs e)
+	protected void OnExitActionActivated(object sender, System.EventArgs e)
 	{
 		Application.Quit();
 	}
--- Tools/SolidReflector/trunk/src/gtk-gui/MainWindow.cs
+++ Tools/SolidReflector/trunk/src/gtk-gui/MainWindow.cs
@@ -11,6 +11,9 @@ public partial class MainWindow
 	private global::Gtk.Action AboutAction;
 	private global::Gtk.VBox vbox1;
 	private global::Gtk.MenuBar MainMenuBar;
+	private global::Gtk.HBox hbox1;
+	private global::Gtk.ScrolledWindow GtkScrolledWindow;
+	private global::Gtk.TreeView assemblyView;
 	
 	protected virtual void Build ()
 	{
@@ -51,11 +54,30 @@ public partial class MainWindow
 		w2.Position = 0;
 		w2.Expand = false;
 		w2.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.hbox1 = new global::Gtk.HBox ();
+		this.hbox1.Name = "hbox1";
+		this.hbox1.Spacing = 6;
+		// Container child hbox1.Gtk.Box+BoxChild
+		this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
+		this.GtkScrolledWindow.Name = "GtkScrolledWindow";
+		this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
+		// Container child GtkScrolledWindow.Gtk.Container+ContainerChild
+		this.assemblyView = new global::Gtk.TreeView ();
+		this.assemblyView.CanFocus = true;
+		this.assemblyView.Name = "assemblyView";
+		this.GtkScrolledWindow.Add (this.assemblyView);
+		this.hbox1.Add (this.GtkScrolledWindow);
+		global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.GtkScrolledWindow]));
+		w4.Position = 0;
+		this.vbox1.Add (this.hbox1);
+		global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.hbox1]));
+		w5.Position = 1;
 		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
-		this.DefaultWidth = 796;
+		this.DefaultWidth = 780;
 		this.DefaultHeight = 515;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
