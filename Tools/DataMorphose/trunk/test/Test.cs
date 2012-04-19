0c9aa0e517c8963300bf68395bf3303683183fae
vgvassilev <v.g.vassilev@gmail.com>
2012-04-19 18:18:10 +0000

    Add basic test infrastructure. Add simple demo database stored in files.

---
+++ Tools/DataMorphose/trunk/test/Test.cs
@@ -0,0 +1,19 @@
+// /*
+//  * $Id: $
+//  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+//  * For further details see the nearest License.txt
+//  */
+using System;
+using NUnit.Framework;
+
+namespace DataMorphose.Test
+{
+  [TestFixture()]
+  public class Test
+  {
+    [Test()]
+    public void TestCase() {
+    }
+  }
+}
+
