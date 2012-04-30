b09bca5fb01d6aa1a6e2e8beb3afd4a18a0dfad6
vgvassilev <v.g.vassilev@gmail.com>
2012-04-30 18:36:33 +0000

    Add basic undo/redo support by introducing reversible actions. Most of the performed transformation should be reversible and easy to undo.

---
+++ Tools/DataMorphose/trunk/src/Controller/ReversibleActions/Action.cs
@@ -0,0 +1,24 @@
+/*
+ * $Id: $
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+using System;
+
+namespace DataMorphose
+{
+  public abstract class Action : IReversibleAction
+  {
+    private string description;
+    public string Description {
+      get { return this.description; }
+      set { description = value; }
+    }
+
+    #region IReversibleAction implementation
+    public abstract void Undo();
+    public abstract void Redo();
+    #endregion
+  }
+}
+
---
+++ Tools/DataMorphose/trunk/src/Controller/ReversibleActions/Actions/SortByColumnAction.cs
@@ -0,0 +1,42 @@
+/*
+ * $Id: $
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+using System.Collections.Generic;
+
+using DataMorphose.Model;
+
+namespace DataMorphose
+{
+  public class SortByColumnAction : Action
+  {
+    private Table table = null;
+    private List<Row> cachedRows = null;
+
+    private Column column;
+
+    public SortByColumnAction(Table table, Column column) {
+      this.table = table;
+      this.column = column;
+    }
+
+    #region implemented abstract members of DataMorphose.Action
+    public override void Undo()
+    {
+      table.Rows = cachedRows;
+    }
+
+    public override void Redo()
+    {
+      table.Rows.Sort(Compare);
+    }
+    #endregion
+
+    private int Compare(Row x, Row y) {
+      if (x.Columns.IndexOf(column))
+      return 0; //if (Row)
+    }
+  }
+}
+
