3e674463ebcaa280450ca7e4d612db7fb228e1c9
vgvassilev <v.g.vassilev@gmail.com>
2012-09-05 09:48:28 +0000

    Add initial implementation of the triplet printout.

--- SolidOpt/trunk/src/Services/Transformations/CodeModel/ThreeAddressCode/src/Triplet.cs
+++ SolidOpt/trunk/src/Services/Transformations/CodeModel/ThreeAddressCode/src/Triplet.cs
@@ -115,5 +115,37 @@ namespace SolidOpt.Services.Transformations.CodeModel.ThreeAddressCode {
         this.operand1 = operand1;
         this.operand2 = operand2;
     }
+
+    public override string ToString ()
+    {
+      System.Text.StringBuilder sb = new System.Text.StringBuilder();
+      if (result != null)
+        sb.AppendFormat("{0} = ", result.ToString());
+      if (operand1 != null)
+        sb.AppendFormat("{0}", operand1.ToString());
+      switch(opcode) {
+        case TripletOpCode.Addition: sb.Append(" +"); break;
+        case TripletOpCode.And: sb.Append(" &&"); break;
+        case TripletOpCode.Assignment: sb.Append(" ="); break;
+        case TripletOpCode.Call: sb.Append(" call"); break;
+        case TripletOpCode.CallVirt: sb.Append(" callvirtual"); break;
+        case TripletOpCode.Division: sb.Append(" /"); break;
+        case TripletOpCode.Equal: sb.Append(" =="); break;
+        case TripletOpCode.Goto: sb.Append(" goto"); break;
+        case TripletOpCode.IfFalse: sb.Append(" ifFalse"); break;
+        case TripletOpCode.IfTrue: sb.Append(" ifTrue"); break;
+        case TripletOpCode.Less: sb.Append(" <"); break;
+        case TripletOpCode.Multiplication: sb.Append(" *"); break;
+        case TripletOpCode.Or: sb.Append(" ||"); break;
+        case TripletOpCode.PushParam: sb.Append(" pushParam"); break;
+        case TripletOpCode.Return: sb.Append(" return"); break;
+        case TripletOpCode.Substraction: sb.Append(" -"); break;
+        default: sb.Append(" UNKNOWN "); break;
+      }
+      if (operand2 != null)
+        sb.AppendFormat("{0}", operand2.ToString());
+
+      return sb.ToString();
+    }
   }
 }
