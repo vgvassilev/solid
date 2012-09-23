4fafd2d55ea5d0cdb1f21f1573b96a220cdde8a2
vgvassilev <v.g.vassilev@gmail.com>
2012-09-23 11:46:27 +0000

    Add export to csv files. Now the model is exported in csv format. Each table is exported in separate file with the name of the table and csv extension. The csvdb file shows the files that are part of the database.

---
+++ Tools/DataMorphose/trunk/src/Controller/Export/CSVExporter.cs
@@ -0,0 +1,27 @@
+/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+
+using System.Text;
+using System.IO;
+
+using DataMorphose.Model;
+
+namespace DataMorphose.Import
+{
+  public class CSVExporter
+  {
+    // TODO: Add documentation and test for that.
+    public void ExportDatabase(Database db, string path) {
+      string dbFile = Path.Combine(path, db.Name + ".csvdb");
+      foreach (Table table in db.Tables) {
+        // Append the contents of the table to a file.
+        File.WriteAllText(Path.Combine(path, table.Name + ".csv"), table.ToString());
+        // Append the file name to the database description file.
+        File.AppendText(dbFile).WriteLine(Path.Combine(db.Name + ".csv"));
+      }
+    }
+  }
+}
