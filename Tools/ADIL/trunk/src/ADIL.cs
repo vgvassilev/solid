e51cbb39072f8ecc1742db45bdcba604a8a799e0
alexander_penev <alexander_penev@yahoo.com>
2012-12-02 10:41:33 +0000

    Add new tools project ADIL - Automatic differenciation of IL methods.

---
+++ Tools/ADIL/trunk/src/ADIL.cs
@@ -0,0 +1,18 @@
+// /*
+//  * $Id:
+//  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+//  * For further details see the nearest License.txt
+//  */
+//
+using System;
+
+namespace SolidOpt.Tools.ADIL
+{
+    public class ADIL
+    {
+        public ADIL() {
+        }
+
+    }
+}
+
---
+++ Tools/ADIL/trunk/test/BaseTests.cs
@@ -0,0 +1,20 @@
+// /*
+//  * $Id:
+//  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+//  * For further details see the nearest License.txt
+//  */
+//
+using System;
+using NUnit.Framework;
+
+namespace SolidOpt.Tools.ADIL.Test
+{
+    [TestFixture()]
+    public class BaseTests
+    {
+        [Test()]
+        public void TestCase() {
+        }
+    }
+}
+
