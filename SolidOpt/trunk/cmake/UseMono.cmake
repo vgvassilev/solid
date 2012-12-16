16bd6c62f17b1e5c0d281765232b53990b21bd63
vgvassilev <v.g.vassilev@gmail.com>
2012-12-16 19:29:21 +0000

    Add initial support for cmake based builds

---
+++ SolidOpt/trunk/cmake/UseMono.cmake
@@ -0,0 +1,16 @@
+#
+# A CMake Module for using Mono.
+#
+# The following variables are set:
+#   (none)
+#
+# Additional references can be found here:
+#   http://www.mono-project.com/Main_Page
+#   http://www.mono-project.com/CSharp_Compiler
+#
+# This file is based on the work of SimpleITK:
+#   https://github.com/SimpleITK/SimpleITK/blob/master/CMake/UseCSharp.cmake
+# Copyright (c) SolidOpt Team
+#
+
+message( STATUS "Using Mono compiler version ${CSHARP_MONO_VERSION}" )
