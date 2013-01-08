ac56d32912fbc457c075502333ae81bc552c1b55
alexander_penev <alexander_penev@yahoo.com>
2013-01-08 08:04:43 +0000

    Fix: Use native paths in solution and project generator.\nFix: Revision and Last date if SVN do not exist.

--- SolidOpt/trunk/Version.cmake
+++ SolidOpt/trunk/Version.cmake
@@ -12,35 +12,32 @@
 set(SolidOpt_Major 0)
 set(SolidOpt_Minor 0)
 
-set(temp "$Revision$")
-string(REGEX REPLACE "^.?Revision:?.?([0-9]*).*$" "\\1" SolidOpt_Revision "${temp}")
-if (NOT SolidOpt_Revision)
-  set (SolidOpt_Revision 0)
-endif ()
+# Get trunk rev number.
+find_program(SVN_EXECUTABLE NAMES svn svn.exe DOC "subversion command line client")
 
-set(temp "$Date$")
-string(REGEX REPLACE "^.?Date:?.?(.*).$" "\\1" SolidOpt_LastDate "${temp}")
-if (NOT SolidOpt_LastDate)
-  set (SolidOpt_LastDate "2013-01-01")
-endif ()
-string(STRIP "${SolidOpt_LastDate}" SolidOpt_LastDate)
+macro(SUBVERSION_GET dir variable name)
+  execute_process(
+    COMMAND ${SVN_EXECUTABLE} info ${dir}
+    OUTPUT_VARIABLE ${variable}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  string(REGEX REPLACE "^(.*\n)?${name}: ([^\n]+).*" "\\2" ${variable} "${${variable}}")
+endmacro(SUBVERSION_GET)
 
-## Get trunk rev number.
-#find_program(SVN_EXECUTABLE NAMES svn svn.exe DOC "subversion command line client")
-#
-#macro(SUBVERSION_GET dir variable name)
-#  execute_process(
-#    COMMAND ${SVN_EXECUTABLE} info ${dir}
-#    OUTPUT_VARIABLE ${variable}
-#    OUTPUT_STRIP_TRAILING_WHITESPACE
-#    )
-#  string(REGEX REPLACE "^(.*\n)?${name}: ([^\n]+).*" "\\2" ${variable} "${${variable}}")
-#endmacro(SUBVERSION_GET)
-#
-#if (SVN_EXECUTABLE)
-#  subversion_get(${CMAKE_SOURCE_DIR} SolidOpt_Revision "Revision")
-#  subversion_get(${CMAKE_SOURCE_DIR} SolidOpt_LastDate "Last Changed Date")
-#else ()
-#  set(SolidOpt_Revision 0)
-#  set(SolidOpt_LastDate 2013)
-#endif ()
+if (SVN_EXECUTABLE)
+  subversion_get(${CMAKE_SOURCE_DIR} SolidOpt_Revision "Revision")
+  subversion_get(${CMAKE_SOURCE_DIR} SolidOpt_LastDate "Last Changed Date")
+else ()
+  set(temp "$Revision$")
+  string(REGEX REPLACE "^.?Revision:?.?([0-9]*).*$" "\\1" SolidOpt_Revision "${temp}")
+  if (NOT SolidOpt_Revision)
+    set (SolidOpt_Revision 0)
+  endif ()
+
+  set(temp "$Date$")
+  string(REGEX REPLACE "^.?Date:?.?(.*).$" "\\1" SolidOpt_LastDate "${temp}")
+  if (NOT SolidOpt_LastDate)
+    set (SolidOpt_LastDate "2013-01-01")
+  endif ()
+  string(STRIP "${SolidOpt_LastDate}" SolidOpt_LastDate)
+endif ()
