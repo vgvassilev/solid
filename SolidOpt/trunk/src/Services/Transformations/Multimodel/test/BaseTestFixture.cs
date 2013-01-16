ac209b930b1aedd4177312550f6059bce1b46a66
vgvassilev <v.g.vassilev@gmail.com>
2013-01-16 20:56:17 +0000

    Style.

--- SolidOpt/trunk/src/Services/Transformations/Multimodel/test/BaseTestFixture.cs
+++ SolidOpt/trunk/src/Services/Transformations/Multimodel/test/BaseTestFixture.cs
@@ -3,6 +3,7 @@
  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
  * For further details see the nearest License.txt
  */
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
