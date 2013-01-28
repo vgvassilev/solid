210020441c5598592577e690eb71da936170cf25
alexander_penev <alexander_penev@yahoo.com>
2013-01-28 10:26:08 +0000

    Add csharp_add_dependencies definition macro. Fix all cmake files to use the new macro.

--- SolidOpt/trunk/cmake/UseCSharp.cmake
+++ SolidOpt/trunk/cmake/UseCSharp.cmake
@@ -15,14 +15,15 @@
 #   CSHARP_VERSION - the version number of the C# compiler (eg. "v4.0.30319")
 #
 # The following macros are defined:
-#   CSHARP_ADD_EXECUTABLE( name references [(ref_files | source_files)*] ) - Define C# executable with the given name
-#   CSHARP_ADD_GUI_EXECUTABLE( name references [(ref_files | source_files)*] ) - Define C# gui executable with the given name
-#   CSHARP_ADD_LIBRARY( name references [(ref_files | source_files)*] ) - Define C# library with the given name
+#   CSHARP_ADD_EXECUTABLE( name source_files* ) - Define C# executable with the given name
+#   CSHARP_ADD_GUI_EXECUTABLE( name source_files* ) - Define C# gui executable with the given name
+#   CSHARP_ADD_LIBRARY( name source_files* ) - Define C# library with the given name
+#
+#   CSHARP_ADD_DEPENDENCIES( name references* ) - Define C# dependencies for project with the given name. For now dependencies must be defined after library.
 #
 # Examples:
-#   CSHARP_ADD_EXECUTABLE( MyExecutable "" "Program.cs" )
-#   CSHARP_ADD_EXECUTABLE( MyExecutable "ref1.dll ref2.dll" "Program.cs File1.cs" )
-#   CSHARP_ADD_EXECUTABLE( MyExecutable "ref1.dll;ref2.dll" "Program.cs;File1.cs" )
+#   CSHARP_ADD_EXECUTABLE( MyExecutable "Program.cs" )
+#   CSHARP_ADD_DEPENDENCIES( MyExecutable "ref1.dll" "ref2.dll" )
 #
 
 # Check something was found
@@ -45,7 +46,9 @@ set_property(GLOBAL PROPERTY target_proj_file_property)
 set_property(GLOBAL PROPERTY target_name_property)
 set_property(GLOBAL PROPERTY target_type_property)
 set_property(GLOBAL PROPERTY target_output_type_property)
+set_property(GLOBAL PROPERTY target_out_property)
 set_property(GLOBAL PROPERTY target_refs_property)
+set_property(GLOBAL PROPERTY target_sources_property)
 set_property(GLOBAL PROPERTY target_sources_dep_property)
 set_property(GLOBAL PROPERTY target_src_dir_property)
 set_property(GLOBAL PROPERTY target_bin_dir_property)
@@ -99,8 +102,6 @@ macro( CSHARP_ADD_PROJECT type name )
     @ONLY
   )
 
-  set( refs "/reference:System.dll" )
-  set( depRefs "System.dll" )
   set( sources )
   set( sources_dep )
 
@@ -124,25 +125,19 @@ macro( CSHARP_ADD_PROJECT type name )
 
   # Step through each argument
   foreach( it ${ARGN} )
-    if( ${it} MATCHES "(.*)(dll)" )
-       # Argument is a dll, add reference
-       list( APPEND refs /reference:${it} )
-       list( APPEND depRefs ${it} )
-    else( )
-      # Argument is a source file
-      if( EXISTS ${it} )
-        list( APPEND sources ${it} )
-        list( APPEND sources_dep ${it} )
-      elseif( EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${it} )
-        list( APPEND sources ${it} )
-        list( APPEND sources_dep ${it} )
-      elseif( ${it} MATCHES "[*]" )
-        # For dependencies, we need to expand wildcards
-        FILE( GLOB it_glob ${it} )
-        list( APPEND sources ${it} )
-        list( APPEND sources_dep ${it_glob} )
-      endif( )
-    endif ( )
+    # Argument is a source file
+    if( EXISTS ${it} )
+      list( APPEND sources ${it} )
+      list( APPEND sources_dep ${it} )
+    elseif( EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${it} )
+      list( APPEND sources ${it} )
+      list( APPEND sources_dep ${it} )
+    elseif( ${it} MATCHES "[*]" )
+      # For dependencies, we need to expand wildcards
+      FILE( GLOB it_glob ${it} )
+      list( APPEND sources ${it} )
+      list( APPEND sources_dep ${it_glob} )
+    endif( )
   endforeach( )
   # Check we have at least one source
   list( LENGTH sources_dep sources_length )
@@ -150,28 +145,6 @@ macro( CSHARP_ADD_PROJECT type name )
     MESSAGE( SEND_ERROR "No C# sources were specified for ${type} ${name}" )
   endif ()
   list( SORT sources_dep )
-
-  # Set up the compiler flag for Debug/Release mode.
-  set(BUILD_TYPE "optimize")
-  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(BUILD_TYPE "debug")
-  endif()
-
-  # Add custom target and command
-  MESSAGE( STATUS "Adding C# ${type} ${name}: '${CSHARP_COMPILER} /t:${output_type} /out:${name}.${output} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}'" )
-  add_custom_command(
-    COMMENT "Compiling C# ${type} ${name}: '${CSHARP_COMPILER} /t:${output_type} /out:${name}.${output} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}'"
-    OUTPUT ${CMAKE_${TYPE_UPCASE}_OUTPUT_DIR}/${name}.${output}
-    COMMAND ${CSHARP_COMPILER}
-    ARGS /t:${output_type} /out:${CMAKE_${TYPE_UPCASE}_OUTPUT_DIR}/${name}.${output} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}
-    WORKING_DIRECTORY ${CMAKE_${TYPE_UPCASE}_OUTPUT_DIR}
-    DEPENDS ${sources_dep}
-  )
-  add_custom_target(
-    ${name} ALL
-    DEPENDS ${CMAKE_${TYPE_UPCASE}_OUTPUT_DIR}/${name}.${output}
-    SOURCES ${sources_dep}
-  )
 
   # Generate project GUID
   find_program(guid_gen NAMES ${CMAKE_RUNTIME_OUTPUT_DIR}/guid.exe)
@@ -191,9 +164,11 @@ macro( CSHARP_ADD_PROJECT type name )
   set_property(GLOBAL APPEND PROPERTY target_name_property "${name}")
   set_property(GLOBAL APPEND PROPERTY target_type_property "${type}")
   set_property(GLOBAL APPEND PROPERTY target_output_type_property "${output_type}")
+  set_property(GLOBAL APPEND PROPERTY target_out_property "${CMAKE_${TYPE_UPCASE}_OUTPUT_DIR}/${name}.${output}")
   set_property(GLOBAL APPEND PROPERTY target_guid_property "${proj_guid}")
-  string(REPLACE ";" "#" r "${depRefs}")
-  set_property(GLOBAL APPEND PROPERTY target_refs_property "#${r}")
+  set_property(GLOBAL APPEND PROPERTY target_refs_property "#System.dll")
+  string(REPLACE ";" "#" s "${sources}")
+  set_property(GLOBAL APPEND PROPERTY target_sources_property "#${s}")
   string(REPLACE ";" "#" sd "${sources_dep}")
   set_property(GLOBAL APPEND PROPERTY target_sources_dep_property "#${sd}")
   set_property(GLOBAL APPEND PROPERTY target_src_dir_property "${CMAKE_CURRENT_SOURCE_DIR}")
@@ -202,13 +177,96 @@ macro( CSHARP_ADD_PROJECT type name )
 
 endmacro( CSHARP_ADD_PROJECT )
 
+# Define dependencies macro
+macro( CSHARP_ADD_DEPENDENCIES name )
+  set(refs)
+
+  # Step through each argument
+  foreach( it ${ARGN} )
+    if( ${it} MATCHES "(.*)(dll)" )
+       list( APPEND refs ${it} )
+    endif ( )
+  endforeach( )
+
+  # Save project references info in global properties
+  get_property(target_name GLOBAL PROPERTY target_name_property)
+  get_property(target_refs GLOBAL PROPERTY target_refs_property)
+  list(FIND target_name ${name} idx)
+  if (idx GREATER -1)
+    if (NOT ("${refs}" STREQUAL ""))
+      string(REPLACE ";" "#" r "${refs}")
+      get_property(target_refs GLOBAL PROPERTY target_refs_property)
+      list(GET target_refs ${idx} old_refs)
+      list(INSERT target_refs ${idx} "${old_refs}#${r}")
+      math(EXPR idx "${idx}+1")
+      list(REMOVE_AT target_refs ${idx})
+      set_property(GLOBAL PROPERTY target_refs_property "${target_refs}")
+    endif()
+  else ()
+    message(WARNING "Project ${name} is not already defined")
+  endif ()
+
+endmacro( CSHARP_ADD_DEPENDENCIES )
+
 # Resolve dependencies
 macro( CSHARP_RESOLVE_DEPENDENCIES )
   # Read global solution info lists
   get_property(target_name GLOBAL PROPERTY target_name_property)
   get_property(target_type GLOBAL PROPERTY target_type_property)
+  get_property(target_output_type GLOBAL PROPERTY target_output_type_property)
+  get_property(target_out GLOBAL PROPERTY target_out_property)
+  get_property(target_sources GLOBAL PROPERTY target_sources_property)
+  get_property(target_sources_dep GLOBAL PROPERTY target_sources_dep_property)
   get_property(target_refs GLOBAL PROPERTY target_refs_property)
 
+  # Set up the compiler flag for Debug/Release mode.
+  set(BUILD_TYPE "optimize")
+  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(BUILD_TYPE "debug")
+  endif()
+
+  # Define custom targets and commands
+  set( i 0 )
+  foreach( name ${target_name} )
+    list( GET target_type ${i} type )
+    list( GET target_output_type ${i} output_type )
+    list( GET target_out ${i} out )
+
+    list( GET target_sources ${i} s )
+    string(SUBSTRING "${s}" 1 -1 s)
+    string(REPLACE "#" ";" sources "${s}")
+
+    list( GET target_sources_dep ${i} sd )
+    string(SUBSTRING "${sd}" 1 -1 sd)
+    string(REPLACE "#" ";" sources_dep "${sd}")
+
+    list( GET target_refs ${i} r )
+    string(REPLACE "#" "#/reference:" r "${r}")
+    string(SUBSTRING "${r}" 1 -1 r)
+    string(REPLACE "#" ";" refs "${r}")
+
+    # Add custom target and command
+    get_filename_component(out_name "${out}" NAME)
+    get_filename_component(out_dir "${out}" PATH)
+    MESSAGE( STATUS "Adding C# ${type} ${name}: '${CSHARP_COMPILER} /t:${output_type} /out:${out_name} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}'" )
+    add_custom_command(
+      COMMENT "Compiling C# ${type} ${name}: '${CSHARP_COMPILER} /t:${output_type} /out:${out_name} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}'"
+      OUTPUT ${out}
+      COMMAND ${CSHARP_COMPILER}
+      ARGS /t:${output_type} /out:${out} /platform:${CSHARP_PLATFORM} /${BUILD_TYPE} ${CSHARP_SDK_COMPILER} ${refs} ${sources}
+      WORKING_DIRECTORY ${out_dir}
+      DEPENDS ${sources_dep}
+    )
+    add_custom_target(
+      ${name} ALL
+      DEPENDS ${out}
+      SOURCES ${sources_dep}
+    )
+
+    math(EXPR i "${i}+1")
+  endforeach( )
+
+  # Resolving all target dependencies
   set( i 0 )
   foreach( name ${target_name} )
     list( GET target_type ${i} type )
@@ -217,13 +275,9 @@ macro( CSHARP_RESOLVE_DEPENDENCIES )
     string(REPLACE "#" ";" refs "${r}")
     MESSAGE( STATUS "Resolving dependencies for ${type}: ${name}" )
     foreach( it ${refs} )
-#      # Argument is a dll, add as dependency. csharp_add_dependency will decide if
-#      # if it was a build target or not.
-#      if( ${it} MATCHES "(.*)(dll)" )
-        # Get the filename only (no slashes)
-        get_filename_component(filename ${it} NAME)
-        csharp_add_dependency(${name} ${filename})
-#      endif( )
+      # Get the filename only (no slashes)
+      get_filename_component(filename ${it} NAME)
+      csharp_add_dependency(${name} ${filename})
     endforeach( )
 
     math(EXPR i "${i}+1")
