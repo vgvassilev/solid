fa96a53c01a83bdb90956dff7d0234e16710dc00
vgvassilev <v.g.vassilev@gmail.com>
2013-02-24 14:09:00 +0000

    Remove useless usings

--- SolidOpt/trunk/src/Services/Transformations/Multimodel/CFGtoIL/src/CFGtoILTransformer.cs
+++ SolidOpt/trunk/src/Services/Transformations/Multimodel/CFGtoIL/src/CFGtoILTransformer.cs
@@ -7,9 +7,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 
 using SolidOpt.Services.Transformations.Multimodel;
 using SolidOpt.Services.Transformations.CodeModel.ControlFlowGraph;
