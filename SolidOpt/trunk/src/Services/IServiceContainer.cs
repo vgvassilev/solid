47358a6acfb472afee151ef090ff5fcfc93cece9
vgvassilev <v.g.vassilev@gmail.com>
2013-04-06 17:17:08 +0000

    Document the IServiceContainer.

--- SolidOpt/trunk/src/Services/IServiceContainer.cs
+++ SolidOpt/trunk/src/Services/IServiceContainer.cs
@@ -8,9 +8,30 @@ using System;
 
 namespace SolidOpt.Services
 {
+  /// <summary>
+  /// Provides and contains services.
+  /// </summary>
+  /// <description>
+  /// A service container could define number of services like the ServiceProvider.
+  /// It could also register other services provided by somebody else (either a 
+  /// service provider or service container).
+  /// </description>
   public interface IServiceContainer : IServiceProvider
   {
+    /// <summary>
+    /// Adds a service.
+    /// </summary>
+    /// <returns><c>true</c>, if service was added, <c>false</c> otherwise.</returns>
+    /// <param name="service">Service.</param>
+    ///
     bool AddService(IService service);
+
+    /// <summary>
+    /// Removes a service.
+    /// </summary>
+    /// <returns><c>true</c>, if service was removed, <c>false</c> otherwise.</returns>
+    /// <param name="service">Service.</param>
+    /// 
     bool RemoveService(IService service);
   }
   
