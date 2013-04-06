46136d71ebfe82b26ddc0ce33f301e57c7e66f9f
vgvassilev <v.g.vassilev@gmail.com>
2013-04-06 16:44:25 +0000

    Document IService.

--- SolidOpt/trunk/src/Services/IService.cs
+++ SolidOpt/trunk/src/Services/IService.cs
@@ -6,8 +6,20 @@
 
 using System;
 
+/// <summary>
+/// Implements SolidOpt's service oriented model.
+/// </summary>
+/// <description>
+/// The model defines three key components:
+///   * A Service - provides an atomic entity.
+///   * A Service Provider - an entity that provides services.
+///   * A Service Container - an entity that contains services and possibly service providers.
+/// </description>
 namespace SolidOpt.Services
 {
+  /// <summary>
+  /// Represents a service provided by SolidOpt.
+  /// </summary>
   public interface IService
   {
   }
