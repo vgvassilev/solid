c0eaf372b5881f45dee90ec581b1613e332cee6d
vgvassilev <v.g.vassilev@gmail.com>
2013-05-25 17:11:50 +0000

    Add some sample class and test case in the skeleton.

---
+++ SolidOpt/trunk/tools/sampleProjectSkeleton/src/SampleClass.cs
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+
+using System;
+
+namespace SolidOpt.Sample
+{
+
+
+  public class SampleClass
+  {
+
+    public SampleClass()
+    {
+    }
+  }
+}
