519e16fed1e739ece233570717c26b3a166021a0
vgvassilev <v.g.vassilev@gmail.com>
2013-05-29 19:59:05 +0000

    Start porting DM to GTK# and cmake, following the sample project located in tools.

---
+++ Tools/DataMorphose/trunk/src/IDataMorphose.cs
@@ -0,0 +1,63 @@
+using System;
+
+using SolidOpt.Services;
+
+namespace DataMorphose
+{
+  /// <summary>
+  /// Exposes methods and events an application has to implement in order to be compatible with
+  /// the SolidReflector/DataMorphose plugins
+  /// </summary>
+  /// 
+  public interface IDataMorphose : IService
+  {
+    /// <summary>
+    /// Occurs when the main application (SolidReflector, DataMorphose) is requested to terminate.
+    /// The event is sent to the subscribed plugins allowing them to do some work before the
+    /// application is terminated.
+    /// </summary>
+    event EventHandler<EventArgs> OnShutDown;
+
+    /// <summary>
+    /// Gets the current user configuration directory.
+    /// </summary>
+    /// <description>
+    /// On Unix it is /home/user/.config/SolidReflector
+    /// On Mac it is /Users/user/.config/SolidReflector
+    /// On Windows it is in AppData
+    /// </description>
+    /// <returns>
+    /// The configuration directory.
+    /// </returns>
+    ///
+    string GetConfigurationDirectory();
+
+    /// <summary>
+    /// Gets the application main window.
+    /// </summary>
+    /// <returns>
+    /// The main window.
+    /// </returns>
+    ///
+    //FIXME: Must return Gtk.Window
+    MainWindow GetMainWindow();
+
+    /// <summary>
+    /// Gets the application main menu.
+    /// </summary>
+    /// <returns>
+    /// The main menu.
+    /// </returns>
+    /// 
+    Gtk.MenuBar GetMainMenu();
+
+    /// <summary>
+    /// Gets the currently loaded plugins in the application.
+    /// </summary>
+    /// <returns>
+    /// The plugins.
+    /// </returns>
+    /// 
+    PluginServiceContainer GetPlugins();
+  }
+}
--- Tools/DataMorphose/trunk/src/Program.cs
+++ Tools/DataMorphose/trunk/src/Program.cs
@@ -3,22 +3,20 @@
  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
  * For further details see the nearest License.txt
  */
-using DataMorphose.GUI;
 
 using System;
-using System.Windows.Forms;
+using Gtk;
 
 namespace DataMorphose
 {
-  internal sealed class Program
+ class MainClass
+ {
+  public static void Main (string[] args)
   {
-    [STAThread]
-    private static void Main(string[] args)
-    {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainForm());
-    }
+   Application.Init();
+   MainWindow win = new MainWindow();
+   win.Show();
+   Application.Run();
   }
+ }
 }
-
