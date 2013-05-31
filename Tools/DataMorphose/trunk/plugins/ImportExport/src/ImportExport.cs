bce0d3117944fa83811de1542c88f6d837e3fb1a
ppetrova <petya.petrova@hotmail.com>
2013-05-31 12:00:10 +0000

    Move Import and Export folders to the plugin src directory. Show dialog window when Import or Export is selected

--- Tools/DataMorphose/trunk/plugins/ImportExport/src/ImportExport.cs
+++ Tools/DataMorphose/trunk/plugins/ImportExport/src/ImportExport.cs
@@ -5,6 +5,9 @@
 //  */
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
 using SolidOpt.Services;
 using MonoDevelop.Components.Docking;
 
@@ -39,13 +42,15 @@ namespace DataMorphose
         mainMenuBar.Append(fileMenu);
       }
         
-      // Setting up the Open menu item in File
+      // Setting up the Importand Export menu item in File          
       Gtk.MenuItem import = new Gtk.MenuItem("Import");
+      import.Activated += OnImportActivated;
       (fileMenu.Submenu as Gtk.Menu).Prepend(import);
       
       Gtk.MenuItem export = new Gtk.MenuItem("Export");
+      export.Activated += OnExportActivated;
       (fileMenu.Submenu as Gtk.Menu).Prepend(export);
-
+      
       // Attaching the current dockItem in the DockFrame
       dockItem = MainWindow.DockFrame.AddItem("AssemblyBrowser");
       dockItem.DrawFrame = true;
@@ -53,11 +58,102 @@ namespace DataMorphose
 
      
     }
+    
 
     void IPlugin.UnInit (object context)
     {
       throw new NotImplementedException ();
     }
+    
+    
+    /// <summary>
+    /// Raises the activated event when the Import menu item is invoked.
+    /// </summary>
+    /// <param name='sender'>
+    /// The Gtk.MenuItem.
+    /// </param>
+    /// <param name='args'>
+    /// Arguments.
+    /// </param>
+    /// 
+    void OnImportActivated(object sender, EventArgs args)
+    {
+      var fc = new Gtk.FileChooserDialog("Choose a file to import", null, 
+                                         Gtk.FileChooserAction.Open, "Cancel", 
+                                         Gtk.ResponseType.Cancel, "Import", Gtk.ResponseType.Accept);
+      try {
+        fc.SelectMultiple = true;
+        fc.SetCurrentFolder(Environment.CurrentDirectory);
+        if (fc.Run() == (int)Gtk.ResponseType.Accept) {
+          // Get the currently loaded files in the tree view
+          List<string> filesLoaded;
+          //GetLoadedFiles(out filesLoaded);
+
+          List<string> filesToLoad = new List<string>();
+          filesToLoad.AddRange(fc.Filenames);
+          for (uint i = 0; i < fc.Filenames.Length; i++)
+            if (filesLoaded.Contains(fc.Filenames[i])) {
+              filesToLoad.Remove(fc.Filenames[i]);
+              ShowMessageGtk(String.Format("File {0} already loaded.", fc.Filenames[i]));
+            }
+          //LoadFilesInTreeView(filesToLoad.ToArray());
+        }
+      } finally {
+        fc.Destroy();
+      }
+    }
+    
+    /// <summary>
+    /// Raises the activated event when the Export menu item is invoked.
+    /// </summary>
+    /// <param name='sender'>
+    /// The Gtk.MenuItem.
+    /// </param>
+    /// <param name='args'>
+    /// Arguments.
+    /// </param>
+    /// 
+    void OnExportActivated(object sender, EventArgs args)
+    {
+      var fc = new Gtk.FileChooserDialog("Save", null, 
+                                         Gtk.FileChooserAction.Open, "Cancel", 
+                                         Gtk.ResponseType.Cancel, "Export", Gtk.ResponseType.Accept);
+      try {
+        fc.SelectMultiple = true;
+        fc.SetCurrentFolder(Environment.CurrentDirectory);
+        if (fc.Run() == (int)Gtk.ResponseType.Accept) {
+          // Get the currently loaded files in the tree view
+          List<string> filesLoaded;
+          //GetLoadedFiles(out filesLoaded);
+
+          List<string> filesToLoad = new List<string>();
+          filesToLoad.AddRange(fc.Filenames);
+          for (uint i = 0; i < fc.Filenames.Length; i++)
+            if (filesLoaded.Contains(fc.Filenames[i])) {
+              filesToLoad.Remove(fc.Filenames[i]);
+              ShowMessageGtk(String.Format("File {0} already loaded.", fc.Filenames[i]));
+            }
+          //LoadFilesInTreeView(filesToLoad.ToArray());
+        }
+      } finally {
+        fc.Destroy();
+      }
+    }
+    
+    
+    /// <summary>
+    /// Shows message box using GTK.
+    /// </summary>
+    /// <param name='msg'>
+    /// The message.
+    /// </param>
+    private void ShowMessageGtk(string msg) {
+      var msgBox = new Gtk.MessageDialog(null, Gtk.DialogFlags.Modal, Gtk.MessageType.Info,
+                                         Gtk.ButtonsType.Ok, msg);
+      msgBox.Run();
+      msgBox.Destroy();
+    }
+    
     #endregion
 
 
