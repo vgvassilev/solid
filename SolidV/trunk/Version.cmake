c399fbb8ceab25e8a1a1787369115b8cc177373a
mvassilev <mrtn.vassilev@gmail.com>
2013-07-15 09:25:11 +0000

    Add initial cmake support

---
+++ SolidV/trunk/Version.cmake
@@ -0,0 +1,10 @@
+# Defaults from SolidOpt build
+include(../../SolidOpt/trunk/Version.cmake)
+
+# Override
+#TODO: Replace SolidOpt with SolidReflector or Project
+set(SolidOpt_Major 0)
+set(SolidOpt_Minor 0)
+
+set(Project_Title "SolidV - Visual library")
+set(Project_Description "SolidV is ...")
