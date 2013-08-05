8eb94593f4c149215edb8820561efc118da0721e
ppetrova <petya.petrova@hotmail.com>
2013-08-05 09:57:12 +0000

    Remove '//' from '// *' in the header.

--- Tools/DataMorphose/trunk/plugins/ImportExport/src/ImportExport.cs
+++ Tools/DataMorphose/trunk/plugins/ImportExport/src/ImportExport.cs
@@ -1,8 +1,8 @@
-// /*
-//  * $Id$
-//  * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
-//  * For further details see the nearest License.txt
-//  */
+/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
 using System;
 using System.IO;
 using System.Collections.Generic;
