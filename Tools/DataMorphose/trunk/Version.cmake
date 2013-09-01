c1621cbae130202f0040f29326a97e57516bcff3
mvassilev <mrtn.vassilev@gmail.com>
2013-09-01 21:43:35 +0000

    Add missing Version.cmake that redirects to cmake/Version.cmake

---
+++ Tools/DataMorphose/trunk/Version.cmake
@@ -0,0 +1,10 @@
+# Defaults from SolidOpt build
+include(${CMAKE_MODULE_PATH}/Version.cmake)
+
+# Override
+#TODO: Replace SolidOpt with DataMrophose or Project
+set(SolidOpt_Major 0)
+set(SolidOpt_Minor 0)
+
+set(Project_Title "DataMorphose ...")
+set(Project_Description "DataMorphose is ...")
