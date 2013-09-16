8adb554ed63aab1706ffdbb21ec75355b88f4d54
ppetrova <petya.petrova@hotmail.com>
2013-09-16 16:05:38 +0000

    Move the DockItems from MainWindow. Add a very basic filter for testing the new functionality. Put the ObjectInspector in a new tab the LeftDockItem. Rename some variables and tidy the code.

--- Tools/DataMorphose/trunk/plugins/Visualizer/src/Visualizer.cs
+++ Tools/DataMorphose/trunk/plugins/Visualizer/src/Visualizer.cs
@@ -23,91 +23,162 @@ namespace DataMorphose.Plugins.Visualizer
   public class Visualizer : IPlugin
   {
     private IDataMorphose morphose = null;
-    private DockItem dockItem = null;
-        
-    private  Toolbar LToolbar;
-
-    private Gtk.Notebook nb = new Gtk.Notebook();
-    private Gtk.VBox vBoxTV = new Gtk.VBox();
+    private Toolbar tbLeft = new Gtk.Toolbar();
+    private Gtk.Notebook nbMiddleDock = new Gtk.Notebook();
+    private Gtk.Notebook nbLeftDock = new Gtk.Notebook();
+    private Gtk.VBox vbRight = new Gtk.VBox();
+    private Gtk.VBox vbMiddle = new Gtk.VBox();
     private Gtk.TreeView treeView = new Gtk.TreeView();
     private Gtk.ComboBox comboBox = new Gtk.ComboBox();
     private SchemaVisualizer schemaV = null;
     private Gtk.DrawingArea drawingArea = null;
-
     private TreeIter iter;
     private string selectedTable = "";
+    private SolidV.Gtk.ObjectInspector propertyGrid;
 
     public Visualizer() {
     }
 
     #region IPlugin implementation
-    void IPlugin.Init (object context)
-    {
+    void IPlugin.Init(object context) {
       morphose = context as IDataMorphose;
 
       morphose.GetModel().ModelChanged += HandleModelChanged;
+         
+      // Left dock item.
+      TuneLeftDockItem();
+      AddDockItem("Tools", nbLeftDock);
+      
+      // Attaching the Visualizer in the DockFrame.
+      AddDockItem("Visualizer", nbMiddleDock);
+      TuneMiddleDockItem();
 
-      // Set up data grid.
+      // Right dock item. 
+      TuneRightDockItem();
+      AddDockItem("Actions", vbRight);
+    }    
+
+    /// <summary>
+    /// Adds the dock item.
+    /// </summary>
+    /// <param name="label">Label.</param>
+    /// <param name="container">Container.</param>
+    private void AddDockItem(string label, Gtk.Container container) {
+      DockItem dockToolbar = morphose.GetMainWindow().DockFrame.AddItem(label);
+      dockToolbar.DrawFrame = true;
+      dockToolbar.Label = label;
+      dockToolbar.Expand = true;
+      dockToolbar.DefaultVisible = true;
+      dockToolbar.Visible = true;
+      dockToolbar.Content = container;
+      container.ShowAll();
+    }
+
+    /// <summary>
+    /// Tunes the left dock item.
+    /// </summary>
+    private void TuneLeftDockItem() {      
+      Gtk.VBox vbox = new Gtk.VBox();
+      tbLeft.Name = "LeftToolbar";
+      vbox.PackStart(tbLeft, true, true, 0);
+      tbLeft.Orientation = Gtk.Orientation.Vertical;
+      // Show only text on the buttons
+      tbLeft.ToolbarStyle = Gtk.ToolbarStyle.Text;
+      
+      nbLeftDock.Add(vbox);;
+      
+      this.propertyGrid = new SolidV.Gtk.ObjectInspector();
+      nbLeftDock.AppendPage(propertyGrid, new Label("Properties"));
+      nbLeftDock.ShowAll();
+    }
+
+    /// <summary>
+    /// Tunes the middle dock item or the data visualizer.
+    /// </summary>
+    private void TuneMiddleDockItem() {
       // Attach the comboBox first.
-      vBoxTV.PackStart(comboBox, false, false, 0);
-      ScrolledWindow scrollTV =  new ScrolledWindow();
+      vbMiddle.PackStart(comboBox, false, false, 0);
+      ScrolledWindow scroll = new ScrolledWindow();
       // Attach the treeView to the scroll bar.
-      scrollTV.AddWithViewport(treeView);
+      scroll.AddWithViewport(treeView);
       // Attach the scrollBar to the box.
-      vBoxTV.PackStart(scrollTV, true, true, 0);
-
-      // Set up DrawingArea tab.
+      vbMiddle.PackStart(scroll, true, true, 0);
+      
+      // Set up DrawingArea.
       drawingArea = new DrawingArea();
       drawingArea.SetSizeRequest(800, 500);
-      ScrolledWindow scrollDA =  new ScrolledWindow();
+      ScrolledWindow scrollDA = new ScrolledWindow();
       Gtk.VBox vBoxDA = new Gtk.VBox();
       Viewport viewPortDA = new Viewport();
       viewPortDA.Add(drawingArea);
       scrollDA.Add(viewPortDA);
       vBoxDA.PackStart(scrollDA, true, true, 0);
-
+      
       // Add Paned container in order to make the two boxes resizable
       Gtk.VPaned splitter = new Gtk.VPaned();
       splitter.Pack1(vBoxDA, true, true);
-      splitter.Pack2(vBoxTV, true, true);
+      splitter.Pack2(vbMiddle, true, true);
       splitter.Position = 400;
       splitter.ShowAll();
 
-      nb.AppendPage(splitter, new Gtk.Label("Schema Visualizer"));
-      schemaV = new SchemaVisualizer(drawingArea);
+      nbMiddleDock.AppendPage(splitter, new Gtk.Label("Schema Visualizer"));
+      nbMiddleDock.ShowAll();
 
+      schemaV = new SchemaVisualizer(drawingArea);
+      
       // Get the selected shape.
       SelectionModel selectionModel = schemaV.Selection;
       selectionModel.ModelChanged += HandleSelectionModelChanged;
-
-      nb.ShowAll();
-      
-      // Attaching the current dockItem in the DockFrame.
-      var mainWindow = morphose.GetMainWindow();
-
-      dockItem = mainWindow.DockFrame.AddItem("Visualizer");
-      dockItem.DrawFrame = true;
-      dockItem.Label = "Visualizer";
-      dockItem.Expand = true;
-      dockItem.DrawFrame = true;
-      dockItem.DefaultVisible = true;
-      dockItem.Visible = true;
-      dockItem.Content = nb;
-
-      // Left toolbar
-      LToolbar = mainWindow.GetToolbar("LeftToolbar"); 
     }
 
-    void IPlugin.UnInit (object context)
-    {
-      throw new NotImplementedException ();
+    /// <summary>
+    /// Tunes the right dock item.
+    /// </summary>
+    private void TuneRightDockItem() {
+      Gtk.Toolbar tb = new Gtk.Toolbar();
+      tb.Name = "RightToolbar";
+      
+      // Add TreeView for the history 
+      Gtk.TreeView historyView = new Gtk.TreeView();
+      Gtk.TreeViewColumn languages = new Gtk.TreeViewColumn();
+      languages.Title = "History";
+      
+      // Write some sample data to see how it looks like
+      Gtk.CellRendererText cell = new Gtk.CellRendererText();
+      languages.PackStart(cell, true);
+      languages.AddAttribute(cell, "text", 0);
+      
+      Gtk.TreeStore treestore = new Gtk.TreeStore(typeof(string));
+      treestore.AppendValues("Import database");
+      treestore.AppendValues("Set primary keys");
+      treestore.AppendValues("Delete row");
+      
+      historyView.AppendColumn(languages);
+      historyView.Model = treestore;
+      
+      // We need to set the expand and fill properties to false, because that way we can resize 
+      // the buttons.
+      vbRight.PackStart(tb, false, false, 0);
+      vbRight.PackStart(historyView, true, true, 0);
+      
+      tb.ToolbarStyle = Gtk.ToolbarStyle.Icons;
+      tb.Orientation = Gtk.Orientation.Horizontal;
+      
+      Gtk.ToolButton undo = new Gtk.ToolButton(Gtk.Stock.Undo);
+      Gtk.ToolButton redo = new Gtk.ToolButton(Gtk.Stock.Redo);
+      tb.Insert(undo, 0);
+      tb.Insert(redo, 1);    
+    }
+
+    void IPlugin.UnInit(object context) {
+      throw new NotImplementedException();
     }    
 
     #endregion
 
     private void HandleModelChanged(object model) {
       DataModel morphoseModel = (DataModel)model;
-      AddLToolbarButtons(morphose.GetModel().DB);
+      AddButtonsToTheLeftToolBar(morphose.GetModel().DB);
       populateComboBox(morphoseModel.DB);
     }
 
@@ -119,10 +190,21 @@ namespace DataMorphose.Plugins.Visualizer
       SelectionModel selectionModel = model as SelectionModel;
       if (selectionModel.Selected.Count != 0) {
         var selected = selectionModel.Selected[0];
-        if (selected is TextBlockShape){
+        // FIXME: Handle when more than one.
+        propertyGrid.CurrentObject = selected;
+        //propertyGrid.Populate();
+        if (selected is TextBlockShape) {
           string title = (selected as TextBlockShape).Title;
           populateGrid(morphose.GetModel().DB.GetTable(title));
-        }
+        } else if (selected is EllipseShape)
+            for (int i = 0; i < schemaV.Scene.Shapes.Count; i++) {
+              Shape block = schemaV.Scene.Shapes[0];
+              if (block is TextBlockShape) {
+                string title = (block as TextBlockShape).Title;
+                ViewFilterResults(new Filter(1, Filter.ConditionRelations.LessThan), 
+                              morphose.GetModel().DB.GetTable(title));
+              }
+            }
       }
     }
 
@@ -130,17 +212,17 @@ namespace DataMorphose.Plugins.Visualizer
     /// Adds buttons that will put a table on the screen.
     /// </summary>
     /// <param name="db">Database</param>
-    private void AddLToolbarButtons(Database db) {
-      for(int i = 0; i < db.Tables.Count; i++) {
+    private void AddButtonsToTheLeftToolBar(Database db) {
+      for (int i = 0; i < db.Tables.Count; i++) {
         Gtk.ToolButton btn = new Gtk.ToolButton(Gtk.Stock.About);        
         btn.Label = db.Tables[i].Name;
-        LToolbar.Insert(btn, i);
+        tbLeft.Insert(btn, i);
         btn.Clicked += OnTableBtnClick;
       }
       Gtk.ToolButton filterBtn = new Gtk.ToolButton(Gtk.Stock.About);
       filterBtn.Label = "Add Filter";
-      LToolbar.Insert(filterBtn, LToolbar.NItems);
-      LToolbar.ShowAll();
+      tbLeft.Insert(filterBtn, tbLeft.NItems);
+      tbLeft.ShowAll();
       filterBtn.Clicked += OnFilterBtnPressed;
     }
 
@@ -163,9 +245,54 @@ namespace DataMorphose.Plugins.Visualizer
     /// <param name="o">O.</param>
     /// <param name="args">Arguments.</param>
     private void OnFilterBtnPressed(object o, EventArgs args) {
-//      morphose.GetModel().DB.Tables[0].Columns[0].Filter 
-//        = new Filter(1, Filter.ConditionRelations.LessThan);
+      morphose.GetModel().DB.Tables[0].Columns[0].Filter 
+        = new Filter(1, Filter.ConditionRelations.LessThan);
       schemaV.DrawFilter();
+    }
+
+    private void ViewFilterResults(Filter filter, Model.Table table) {
+      if (filter.Condition == 1) {
+
+        Gtk.ListStore store = treeView.Model as Gtk.ListStore;
+        
+        // Empty the store on every new table-selection.
+        if (store != null) {
+          foreach (Gtk.TreeViewColumn col in treeView.Columns)
+            treeView.RemoveColumn(col);
+          store.Clear();
+        }
+        
+        // Construct the array of types which is the size of the columns.
+        Type[] types = new Type[table.Columns.Count];
+        for (int i = 0, e = table.Columns.Count; i < e; ++i)
+          types[i] = typeof(string);
+        
+        // The list store must take array of types which it will hold.
+        store = new Gtk.ListStore(types);
+        treeView.Model = store;
+        
+        // Put columns and cell renderers in the ListStore. There we will put the actual values.
+        for (int i = 0, e = table.Columns.Count; i < e; ++i) {
+
+          Model.Column dcol = table.Columns[i];
+          Gtk.TreeViewColumn col = new Gtk.TreeViewColumn();
+          Gtk.CellRendererText cell = new Gtk.CellRendererText();
+          col.PackStart(cell, true);
+          col.AddAttribute(cell, "text", i);
+          col.Title = dcol.Meta.Name;
+          treeView.AppendColumn(col);
+
+        }
+        // Now fill in the rows.
+        for (int i = 0; i < table.GetAsRows().Count; i++) {
+          Model.Row r = table.GetAsRows()[i];
+          if (table.Columns[0].Values[i].ToString().Contains("1")) {
+            store.AppendValues(r.GetAsStringArray());
+          }
+        }
+        treeView.ShowAll();
+      }    
+
     }
 
     /// <summary>
@@ -177,14 +304,14 @@ namespace DataMorphose.Plugins.Visualizer
 
       // Empty the store on every new table-selection.
       if (store != null) {
-        foreach(Gtk.TreeViewColumn col in treeView.Columns)
+        foreach (Gtk.TreeViewColumn col in treeView.Columns)
           treeView.RemoveColumn(col);
         store.Clear();
       }
 
       // Construct the array of types which is the size of the columns.
       Type[] types = new Type[table.Columns.Count];
-      for(int i = 0, e = table.Columns.Count; i < e; ++i)
+      for (int i = 0, e = table.Columns.Count; i < e; ++i)
         types[i] = typeof(string);
 
       // The list store must take array of types which it will hold.
@@ -203,7 +330,7 @@ namespace DataMorphose.Plugins.Visualizer
       }
 
       // Now fill in the rows.
-      foreach(Model.Row r in table.GetAsRows()) {
+      foreach (Model.Row r in table.GetAsRows()) {
         store.AppendValues(r.GetAsStringArray());
       }
       treeView.ShowAll();
@@ -234,7 +361,7 @@ namespace DataMorphose.Plugins.Visualizer
         return;
 
       if (combo.GetActiveIter(out iter)) 
-        selectedTable = (string) combo.Model.GetValue(iter, 0);
+        selectedTable = (string)combo.Model.GetValue(iter, 0);
 
       populateGrid(morphose.GetModel().DB.GetTable(selectedTable));
     }
