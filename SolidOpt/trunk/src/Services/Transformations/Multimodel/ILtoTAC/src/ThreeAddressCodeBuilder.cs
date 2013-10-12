136fcbb894d39c676cd53393c1f3f54bf591896f
vgvassilev <v.g.vassilev@gmail.com>
2013-10-12 12:08:20 +0000

    Add clarification comments.

--- SolidOpt/trunk/src/Services/Transformations/Multimodel/ILtoTAC/src/ThreeAddressCodeBuilder.cs
+++ SolidOpt/trunk/src/Services/Transformations/Multimodel/ILtoTAC/src/ThreeAddressCodeBuilder.cs
@@ -1331,6 +1331,32 @@ namespace SolidOpt.Services.Transformations.Multimodel.ILtoTAC
             // or unaligned. prefix instruction.
             //
 //          case Code.No:
+            // [ECMA-335: 2.2] no. – (prefix) possibly skip a fault check.
+            //                       The specified fault check(s) normally performed as part of the execution 
+            //                       of the subsequent instruction can/shall be skipped.
+            //
+            // stack transition: ...,  -> ...,
+            //
+            // This prefix indicates that the subsequent instruction need not perform the specified fault 
+            // check when it is executed. The byte that follows the instruction code indicates which checks 
+            // can optionally be skipped. This instruction is not verifiable.
+            //
+            // The prefix can be used in the following circumstances:
+            //   0x01: typecheck (castclass, unbox, ldelema, stelem, stelem). The CLI can optionally skip 
+            //         any type checks normally performed as part of the execution of the subsequent 
+            //         instruction. InvalidCastException can optionally still be thrown if the check would 
+            //         fail.
+            //   0x02: rangecheck (ldelem.*, ldelema, stelem.*). The CLI can optionally skip any array range
+            //         checks normally performed as part of the execution of the subsequent instruction. 
+            //         IndexOutOfRangeException can optionally still be thrown if the check would fail.
+            //   0x04: nullcheck (ldfld, stfld, callvirt, ldvirtftn, ldelem.*, stelem.*, ldelema). The CLI
+            //         can optionally skip any null-reference checks normally performed as part of the 
+            //         execution of the subsequent instruction. NullReferenceException can optionally still
+            //         be thrown if the check would fail.
+            //
+            // The byte values can be OR-ed; e.g.; a value of 0x05 indicates that both typecheck and 
+            // nullcheck can optionally be omitted.
+            //
 //          case Code.Rethrow: //tested in Rethrow.il
             // [ECMA-335: 4.24] rethrow – rethrow the current exception.
             //
