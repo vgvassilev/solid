08fc21d7f5a88901580d74271b913d20761a1a58
Martin Vassilev <mrtn.vassilev@gmail.com>
2015-05-11 10:28:09 +0300

    Add labels for displaying additional information related to shapes. These labels will be used for example to denote IfTrue/IfFalse branches between BasicBlocks.

---
+++ SolidV/src/MVC/View/VisualCodeViewers/LabelViewer.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+using System;
+using Cairo;
+
+using SolidV.Cairo;
+
+namespace SolidV.MVC
+{
+  public class LabelViewer : ShapeViewer
+  {
+    public LabelViewer() { }
+
+    public override void DrawShape(IView<Context, Model> view, Context context, Shape shape)
+    {
+      Label label = (Label)shape;
+      ArrowShape arrow = (ArrowShape)label.Parent;
+      Glue fromGLue = arrow.FromGlue;
+      Glue toGlue = arrow.ToGlue;
+
+      context.Save();
+      context.Matrix = shape.Matrix;
+
+      PointD p1 = (fromGLue != null) ?
+        fromGLue.TransformPointToGlobal(fromGLue.Center, context) : arrow.From.Center;
+      p1 = label.TransformPointToLocal(p1, context);
+
+      PointD p2 = (toGlue != null) ?
+        toGlue.TransformPointToGlobal(toGlue.Center, context) : arrow.To.Center;
+      p2 = label.TransformPointToLocal(p2, context);
+
+
+
+      double x = Math.Min(p1.X, p2.X) + Math.Abs(p1.X - p2.X)/2;
+      double y = Math.Min(p1.Y, p2.Y) + Math.Abs(p1.Y - p2.Y)/2;
+
+      PointD p = new PointD(x, y);
+
+      context.MoveTo(p);
+
+      context.ShowText(label.TextLabel);
+
+      context.Restore();
+      if (view.Mode == ViewMode.Render) {
+        Console.WriteLine(string.Format("arrow X: {0}, arrow Y: {1}", p.X.ToString(), p.Y.ToString()));
+      }
+    }
+  }
+}
+
