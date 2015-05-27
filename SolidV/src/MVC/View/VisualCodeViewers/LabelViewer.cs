b436f67303f038cf3c88a53502b373f1bf96a5a5
Martin Vassilev <mrtn.vassilev@gmail.com>
2015-05-27 14:04:17 +0300

    Take into account the current view mode.

--- SolidV/src/MVC/View/VisualCodeViewers/LabelViewer.cs
+++ SolidV/src/MVC/View/VisualCodeViewers/LabelViewer.cs
@@ -19,26 +19,28 @@ namespace SolidV.MVC
       Label lb = (Label)shape;
       ArrowShape arrow = (ArrowShape)lb.Parent;
 
-      context.Save();
-      context.Matrix = shape.Matrix;
+      if (view.Mode == ViewMode.Render) {
+        context.Save();
+        context.Matrix = shape.Matrix;
 
-      PointD pointFrom = (arrow.FromGlue != null) ?
-        arrow.FromGlue.TransformPointToGlobal(arrow.FromGlue.Center, context) : arrow.From.Center;
-      pointFrom = lb.TransformPointToLocal(pointFrom, context);
+        PointD pointFrom = (arrow.FromGlue != null) ?
+          arrow.FromGlue.TransformPointToGlobal(arrow.FromGlue.Center, context) : arrow.From.Center;
+        pointFrom = lb.TransformPointToLocal(pointFrom, context);
 
-      PointD pointTo = (arrow.ToGlue != null) ?
-        arrow.ToGlue.TransformPointToGlobal(arrow.ToGlue.Center, context) : arrow.To.Center;
-      pointTo = lb.TransformPointToLocal(pointTo, context);
+        PointD pointTo = (arrow.ToGlue != null) ?
+          arrow.ToGlue.TransformPointToGlobal(arrow.ToGlue.Center, context) : arrow.To.Center;
+        pointTo = lb.TransformPointToLocal(pointTo, context);
 
-      double x = Math.Min(pointFrom.X, pointTo.X) + Math.Abs(pointFrom.X - pointTo.X)/2;
-      double y = Math.Min(pointFrom.Y, pointTo.Y) + Math.Abs(pointFrom.Y - pointTo.Y)/2;
+        double x = Math.Min(pointFrom.X, pointTo.X) + Math.Abs(pointFrom.X - pointTo.X)/2;
+        double y = Math.Min(pointFrom.Y, pointTo.Y) + Math.Abs(pointFrom.Y - pointTo.Y)/2;
 
-      PointD p = new PointD(x, y);
+        PointD p = new PointD(x, y);
 
-      context.MoveTo(p);
-      context.ShowText(lb.TextLabel);
+        context.MoveTo(p);
+        context.ShowText(lb.TextLabel);
 
-      context.Restore();
+        context.Restore();
+      }
     }
   }
 }
