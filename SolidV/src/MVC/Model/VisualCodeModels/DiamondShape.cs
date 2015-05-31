9bd26ccec304e5a73ec66acddad67f727c5215db
Alexander Penev <alexander_penev@yahoo.com>
2015-05-31 17:40:56 +0300

    Add new Diamond shape model and viewer. Shapes and Viewers are already SolidOpt services for better discovery.

---
+++ SolidV/src/MVC/Model/VisualCodeModels/DiamondShape.cs
@@ -0,0 +1,25 @@
+/*
+ * $Id$
+ * It is part of the SolidOpt Copyright Policy (see Copyright.txt)
+ * For further details see the nearest License.txt
+ */
+using System;
+using System.Collections.Generic;
+
+using Cairo;
+
+namespace SolidV.MVC
+{
+  [Serializable]
+  public class DiamondShape : Shape, IGluesProvider
+  {
+    public DiamondShape(Rectangle rectangle) : base(rectangle) {}
+
+    IEnumerable<Glue> IGluesProvider.GetGlues() {
+      yield return new ConnectorGluePoint(new PointD(Location.X + Width/2, Location.Y)).SetParent(this);
+      yield return new ConnectorGluePoint(new PointD(Location.X + Width/2, Location.Y + Height)).SetParent(this);
+      yield return new ConnectorGluePoint(new PointD(Location.X, Location.Y + Height/2)).SetParent(this);
+      yield return new ConnectorGluePoint(new PointD(Location.X + Width, Location.Y + Height/2)).SetParent(this);
+    }
+  }
+}
